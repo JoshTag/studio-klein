44a5c7fa84750ea36b6616320a4cc0fd12280748
Joshua Taguicana <joshtaguicana@gmail.com>
2021-03-25 11:18:43 -0400

    adjusted gsap scroll trigger placement

--- utils/gsap.js
+++ utils/gsap.js
@@ -20,9 +20,9 @@ export const projectAsideTransition = (
       scrollTrigger: {
         trigger: triggerStart,
         toggleActions: `${actions}`,
-        start: `${start} center`,
+        start: `${start} center-=100`,
         endTrigger: triggerEnd,
-        end: `${endPlacement} center`,
+        end: `${endPlacement} center-=100`,
       },
     }
   );
@@ -38,9 +38,9 @@ export const projectAsideTransition = (
       scrollTrigger: {
         trigger: triggerStart,
         toggleActions: "play reset play reset",
-        start: `${start} center`,
+        start: `${start} center-=100`,
         endTrigger: triggerEnd,
-        end: `${endPlacement} center`,
+        end: `${endPlacement} center-=100`,
       },
     }
   );
