10712d7b0b092edc6ffbe3f85155bc2a55e26a8f
Joshua Taguicana <joshtaguicana@gmail.com>
2021-06-15 11:57:08 -0400

    added instagram to footer and updated social lottie

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,48 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const FooterContainer = styled.section`
   display: grid;
   place-items: center;
-  height: 70px;
+  height: 100px;
   background: ${({ theme }) => theme.colours.grey};
-`
+`;
 
 const Copyright = styled.small`
   color: #fff;
   font-size: 16px;
-`
+  display: block;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const Social = styled.a`
+  color: #fff;
+  font-size: 16px;
+  text-decoration: none;
+  display: block;
+  text-align: center;
+  width: 20px;
+  height: 20px;
+  margin: 0 auto;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const Footer = () => (
   <FooterContainer>
-    <Copyright>&copy; 2021, The Studio Klein</Copyright>
+    <div>
+      <Copyright>&copy; 2021, The Studio Klein</Copyright>
+      <Social
+        href='https://www.instagram.com/thestudioklein'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img src="/images/instagram.svg"/>
+      </Social>
+    </div>
   </FooterContainer>
 );
 
